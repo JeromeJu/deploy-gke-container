4c47425f3ba01e097a92db9c19e6c88d2ac81249
Jerome Ju <jeromeju@google.com>
2024-03-12 12:20:20 -0400

    v0.4

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ app_name="$2"
 namespace="$3"
 expose_port="$4"
 
-gke-deploy run -i $image -a $app_name -l -n $namespace -x $expose_port
+gke-deploy run -i $image -a $app_name -n $namespace -x $expose_port
