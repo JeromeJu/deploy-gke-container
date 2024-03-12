fe85541b66a1e1bf8c0171e82d92651e6a6b43d5
Jerome Ju <jeromeju@google.com>
2024-03-12 12:09:13 -0400

    v0.2

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ project_id="$5"
 namespace="$6"
 expose_port="$7"
 
-gke-deploy run -i image -a app_name -l region -c cluster_name -p project_id -n namespace -x expose_port
+gke-deploy run -i $image -a $app_name -l $region -c $cluster_name -p $project_id -n $namespace -x $expose_port
