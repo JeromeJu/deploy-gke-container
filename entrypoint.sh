61591ab6692f51ad81b0e7ffea2317e26b068f23
Jerome Ju <jeromeju@google.com>
2024-03-27 22:07:43 -0400

    update var handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,29 @@
 #!/bin/sh -l
 
+gha_version="0.1.0"
+
 image="$1"
 app_name="$2"
-region="$3"
+location="$3"
 cluster_name="$4"
 project_id="$5"
 namespace="$6"
 expose_port="$7"
 
-gke-deploy run -i $image -a $app_name -l $region -c $cluster_name -p $project_id -n $namespace -x $expose_port
+if [ -z "$image" ] || [ -z "$app_name" ] || [ -z "$location" ] || [ -z "$cluster_name" ] || [ -z "$project_id" ]; then
+  echo "Error: Required variables (image, app_name, location, cluster_name, project_id) are not set."
+  exit 1
+fi
+
+# Utilize Google APIs user agent for metrics with the following unique identifier:
+export GOOGLE_APIS_USER_AGENT=google-github-action:deploy-gke/$gha_version
+
+if [ -z "$namespace" ] && [ -z "$expose_port" ]; then
+  gke-deploy run -i "$image" -a "$app_name" -l "$location" -c "$cluster_name" -p "$project_id"
+elif [ -z "$namespace" ] && [ -n "$expose_port" ]; then 
+  gke-deploy run -i "$image" -a "$app_name" -l "$location" -c "$cluster_name" -p "$project_id" -x "$expose_port" 
+elif [ -n "$namespace" ] && [ -z "$expose_port" ]; then 
+  gke-deploy run -i "$image" -a "$app_name" -l "$location" -c "$cluster_name" -p "$project_id" -n "$namespace"
+else
+  gke-deploy run -i "$image" -a "$app_name" -l "$location" -c "$cluster_name" -p "$project_id" -n "$namespace" -x "$expose_port" 
+fi
